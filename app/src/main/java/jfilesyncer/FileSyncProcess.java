e2d21cc5868a350d3adc02eafb64a1d4968081f5
Filip Jukic <filip.jukic@outlook.de>
2023-11-19 23:13:33 +0100

    First commit

---
+++ app/src/main/java/jfilesyncer/FileSyncProcess.java
@@ -0,0 +1,19 @@
+package jfilesyncer;
+
+import java.io.File;
+
+public class FileSyncProcess {
+
+
+    private File source;
+    private File destination;
+
+    public FileSyncProcess(File source, File destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public void doSync() {
+
+    }
+}
